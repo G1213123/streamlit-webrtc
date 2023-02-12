78dbe5a23cdea067e405f4c287a77d7e676073ce
G1213123 <ngwingfung2136@yahoo.com.hk>
2023-02-12 23:22:03 +0800

    v1.7 patch

--- main.py
+++ main.py
@@ -552,7 +552,7 @@ def video_object_detection(variables):
         passing_counter = st_counter_setup_container( image, width, height )
 
         # size limited by streamlit cloud service (superseded)
-        if width > 1920 or height > 1080:
+        if max(width, height) > 1920 or min(width, height) > 1080:
             st.warning( f"File resolution [{width}x{height}] exceeded limit [1920x1080], "
                         f"please consider scale down the video", icon="⚠️" )
         else:
